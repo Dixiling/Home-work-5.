5d80d821c2d902099fc7e000f5a2cd787acd196b
Конпаку <sputnik-999-mel@mail.ru>
2024-02-15 15:02:35 +0500

    vien

---
+++ HM/view/StudentView.java
@@ -0,0 +1,8 @@
+package HM.view;
+import HM.model.*;
+
+public class StudentView {
+    public void printStudent (Student student) {
+        System.out.println(student);
+    }
+}
---
+++ HM/view/TeacherView.java
@@ -0,0 +1,9 @@
+package HM.view;
+
+import HM.model.Teacher;
+
+public class TeacherView {
+     public void printOnConsole(Teacher teacher){
+        System.out.println(teacher.toString());
+    }
+}
